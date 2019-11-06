0ecac3df93b757b959c5f01f5fe0200cb7a3ab51
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-06 13:45:13 +0800

    Fix linter errors

--- mongodb.js
+++ mongodb.js
@@ -5,15 +5,17 @@ const { MongoClient } = mongodb;
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-MongoClient.connect(connectionUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-  if (error) {
-    return console.log({ error });
-  }
+MongoClient.connect(connectionUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log({ error });
+    }
 
-  const db = client.db(databaseName);
+    const db = client.db(databaseName);
 
-  db.collection('users').insertOne({
-    name: 'Ryan',
-    age: 39,
+    db.collection('users').insertOne({
+      name: 'Ryan',
+      age: 39,
+    });
   });
-});
