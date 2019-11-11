7c282bfbec4c5f43da7bb67b1200ef9048c0082b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-11 16:04:08 +0800

    Validate and sanitize inputs

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
   useNewUrlParser: true,
@@ -6,31 +7,63 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
   useUnifiedTopology: true,
 });
 
-// const User = mongoose.model('User', {
-//   name: {
-//     type: String,
-//   },
-//   age: {
-//     type: Number,
-//   },
-// });
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Please put a valid email');
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('You cannot use the word password as password');
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age cannot be negative');
+      }
+    },
+  },
+});
 
-// const brandon = new User({ name: true, age: 'twenty-one' });
+// const brandon = new User({ name: 'Brandon Ingram  ', email: 'BRANDON@mail.com  ', password: 'supermanisawesome' });
 
 // brandon.save().then(user => console.log(user)).catch(e => console.log(e));
 
 const Task = mongoose.model('Task', {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
-const task = new Task({
-  description: 'Learn about cryptocurrencies and Bitcoin',
-  completed: false,
-});
+// const task = new Task({
+//   description: '      Learn about cryptocurrencies and Bitcoin',
+// });
 
-task.save().then(() => console.log(task)).catch(e => console.log(e));
+// task.save().then(() => console.log(task)).catch(e => console.log(e));
