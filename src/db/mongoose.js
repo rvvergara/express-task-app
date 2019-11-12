ec9cec5d80b6a24f45ace05562841112b6c6a3f0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-12 15:59:09 +0800

    Create User model file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,59 +1,7 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-});
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Please put a valid email');
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('You cannot use the word password as password');
-      }
-    },
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age cannot be negative');
-      }
-    },
-  },
-});
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
 });
