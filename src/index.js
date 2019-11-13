d96a896ae7d82c3155232194fcf6bb8ce06aa8e0
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-13 13:51:13 +0800

    Add task show route

--- src/index.js
+++ src/index.js
@@ -12,49 +12,39 @@ app.use(express.json());
 app.post('/users', (req, res) => {
   const user = new User(req.body);
 
-  user.save()
+  user
+    .save()
     .then(newUser => {
-      res
-        .status(201)
-        .json(newUser);
+      res.status(201).json(newUser);
     })
     .catch(({ errors }) => {
-      res
-        .status(422)
-        .json(errors);
+      res.status(422).json(errors);
     });
 });
 
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
 
-  task.save()
+  task
+    .save()
     .then(newTask => {
-      res
-        .status(201)
-        .json(newTask);
+      res.status(201).json(newTask);
     })
     .catch(error => {
-      res
-        .status(422)
-        .json(error.errors);
+      res.status(422).json(error.errors);
     });
 });
 
 app.get('/users', (req, res) => {
-  User
-    .find()
+  User.find()
     .then(users => res.send(users))
     .catch(e => {
-      res
-        .status(500)
-        .send(e);
+      res.status(500).send(e);
     });
 });
 
 app.get('/users/:id', (req, res) => {
-  User
-    .findOne({ _id: req.params.id })
+  User.findOne({ _id: req.params.id })
     .then(user => {
       if (!user) {
         res.status(404);
@@ -63,19 +53,23 @@ app.get('/users/:id', (req, res) => {
       res.json(user);
     })
     .catch(e => {
-      res.status(400)
-        .json(e);
+      res.status(400).json(e);
     });
 });
 
 app.get('/tasks', (req, res) => {
-  Task
-    .find()
+  Task.find()
     .then(tasks => res.json(tasks))
     .catch(e => {
-      res
-        .status(500)
-        .json(e);
+      res.status(500).json(e);
+    });
+});
+
+app.get('/tasks/:id', (req, res) => {
+  Task.findOne({ _id: req.params.id })
+    .then(task => res.json(task))
+    .catch(error => {
+      res.status(404).json(error);
     });
 });
 
