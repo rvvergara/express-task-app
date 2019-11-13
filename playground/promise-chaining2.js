2ba37553439667e69ecc22b790f55f2a05278455
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-13 17:19:54 +0800

    Convert promise-chaining2 into async/await

--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,6 +1,14 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5dcba9cc4c748f574b49b6c0')
-  .then(() => Task.countDocuments({ completed: false }))
-  .then(count => console.log(count));
+// Task.findByIdAndDelete('5dcba9cc4c748f574b49b6c0')
+//   .then(() => Task.countDocuments({ completed: false }))
+//   .then(count => console.log(count));
+
+const deleteAndCountIncomplete = async (id) => {
+  await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteAndCountIncomplete('5dcbca79a3403a6003a1cc21').then(count => console.log('NEW COUNT', count)).catch(e => console.log(e));
