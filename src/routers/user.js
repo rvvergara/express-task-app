f7ccc3a107de035b166192806c9b19b636ff99a4
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-14 13:51:10 +0800

    Create users controller

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,66 +1,16 @@
 const express = require('express');
-const User = require('../models/user');
+const usersController = require('../controllers/users');
 
 const router = new express.Router();
 
-router.post('/users', async (req, res) => {
-  const user = new User(req.body);
-  try {
-    const newUser = await user.save();
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(422).json(err.errors);
-  }
-});
+router.post('/users', usersController.create);
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find();
-    res.send(users);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-});
+router.get('/users', usersController.index);
 
-router.get('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    return user
-      ? res.json(user)
-      : res.status(404).json({ error: 'Cannot find user' });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+router.get('/users/:id', usersController.show);
 
-router.put('/users/:id', async (req, res) => {
-  const allowedUpdates = ['name', 'email', 'age', 'password'];
-  const updates = Object.keys(req.body);
-  const isValid = updates.every(update => allowedUpdates.includes(update));
+router.put('/users/:id', usersController.update);
 
-  if (!isValid) {
-    return res.status(422).json({ error: 'Disallowed property/ies' });
-  }
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true, runValidators: true,
-    });
-
-    return user
-      ? res.status(200).json(user)
-      : res.status(404).json({ error: 'Cannot find user' });
-  } catch (e) {
-    res.status(422).send(e.errors);
-  }
-});
-
-router.delete('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    return user ? res.status(202).json({ message: 'Successfully deleted user' }) : res.status(404).json({ error: 'Cannot find user' });
-  } catch (error) {
-    res.json(error);
-  }
-});
+router.delete('/users/:id', usersController.delete);
 
 module.exports = router;
