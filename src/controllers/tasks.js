995cc22c33d92c68983677f15b69ae8f898b8820
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-11-20 19:06:50 +0800

    Improved task show and update actions
    - Created a findTask private method that will find a task using ObjectID
    from mongoose
    - Use the method on update and show actions
    - Convert every method as a property in exports object

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,67 +1,71 @@
+const { ObjectId } = require('mongoose').Types;
 const Task = require('../models/task');
 
-module.exports = {
-  async index(req, res) {
-    try {
-      await req.user.populate('tasks').execPopulate();
-      const { tasks } = req.user;
-      res.json(tasks);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  },
+const findTask = async (user, id) => {
+  await user.populate('tasks').execPopulate();
+  return user.tasks.find(({ _id }) => ObjectId(_id).toHexString() === id);
+};
 
-  async show(req, res) {
-    try {
-      const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
-      return task
-        ? res.json(task)
-        : res.status(404).json({ error: 'Cannot find task' });
-    } catch (error) {
-      res.json(error);
-    }
-  },
+exports.index = async (req, res) => {
+  try {
+    await req.user.populate('tasks').execPopulate();
+    const { tasks } = req.user;
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-  async create(req, res) {
-    const task = new Task({
-       ...req.body,
-       owner: req.user._id,
+exports.show = async (req, res) => {
+  try {
+    const task = await findTask(req.user, req.params.id);
+    return task
+      ? res.json(task)
+      : res.status(404).json({ error: 'Cannot find task' });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+exports.create = async (req, res) => {
+  const task = new Task({
+      ...req.body,
+      owner: req.user._id,
 });
-    try {
-      const newTask = await task.save();
-      res.status(201).json(newTask);
-    } catch (err) {
-      res.status(422).json(err.errors);
-    }
-  },
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(422).json(err.errors);
+  }
+};
 
-  async update(req, res) {
-    const allowedUpdates = ['description', 'completed'];
-    const updates = Object.keys(req.body);
-    const isValid = updates.every(update => allowedUpdates.includes(update));
+exports.update = async (req, res) => {
+  const allowedUpdates = ['description', 'completed'];
+  const updates = Object.keys(req.body);
+  const isValid = updates.every(update => allowedUpdates.includes(update));
 
-    if (!isValid) {
-      return res.status(422).json({ error: 'Disallowed property/ies' });
-    }
+  if (!isValid) {
+    return res.status(422).json({ error: 'Disallowed property/ies' });
+  }
 
-    try {
-      const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
-      updates.forEach(update => task[update] = req.body[update]);
-      await task.save();
+  try {
+    const task = await findTask(req.user, req.params.id);
+    updates.forEach(update => task[update] = req.body[update]);
+    await task.save();
 
-      return task ? res.status(202).json(task) : res.status(404).json({ error: 'Cannot find task' });
-    } catch (e) {
-      res.status(422).json(e.errors || e);
-    }
-  },
+    return task ? res.status(202).json(task) : res.status(404).json({ error: 'Cannot find task' });
+  } catch (e) {
+    res.status(422).json(e.errors || e);
+  }
+};
 
-  async delete(req, res) {
-    try {
-      const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+exports.delete = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
-      return task ? res.status(202).json({ message: 'Task deleted' }) : res.status(404).json({ error: 'Cannot find task' });
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  },
+    return task ? res.status(202).json({ message: 'Task deleted' }) : res.status(404).json({ error: 'Cannot find task' });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
